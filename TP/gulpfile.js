2ffbc86883d0fa7e4cddfd0cbd3e843ebaef0b24
Pablo Valor <pablovalor89@gmail.com>
2016-09-20 20:15:03 -0300

    Se agregan modulos de gulp para manejo de css's

--- TP/gulpfile.js
+++ TP/gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
-var path = require('path');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var fs = require('fs');
+var path 			= require('path'),
+	gulp 			= require('gulp'),
+	sass 			= require('gulp-sass'),
+	concatFiles 	= require('gulp-concat'),
+	cssmin 			= require('gulp-cssmin'),
+	rename			= require('gulp-rename'),
+	uglify 			= require('gulp-uglify');
 
 var paths = {
 	css: {
-		src: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\css\\main.scss',
+		src: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\css',
 		dest: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\css\\dist',
 	},
 	js: {
@@ -27,11 +30,15 @@ var paths = {
 
 
 gulp.task('sass', function () {
-  return gulp.src(paths.css.src)
+  return gulp.src(paths.css.src + '\\main.scss')
     .pipe(sass().on('error', sass.logError))
+	.pipe(cssmin({ keepSpecialComments: 0 }))
+	.pipe(rename({ suffix: '.min'}))
     .pipe(gulp.dest(paths.css.dest));
 });
 
-gulp.task('default', function(){
-	return console.log('hola');
+gulp.task('watch', function(){
+	gulp.watch(paths.css.src + '\\*.scss', ['sass']);
 });
+
+gulp.task('default', ['sass', 'watch']);
