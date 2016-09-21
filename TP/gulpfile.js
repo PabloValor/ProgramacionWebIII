7c7279e2fa386344e0ff9fae6ceabaf72c1335e3
Pablo Valor <pablovalor89@gmail.com>
2016-09-20 21:07:38 -0300

    Se agregan tasks para menejo de js de terceros

--- TP/gulpfile.js
+++ TP/gulpfile.js
@@ -9,36 +9,51 @@ var path 			= require('path'),
 
 var paths = {
 	css: {
+		vendor: {
+			bootstrap: 'D:\\repos\\ProgramacionWebIII\\TP\\bower_components\\bootstrap\\dist\\css\\boostrap.css'
+		},
 		src: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\css',
 		dest: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\css\\dist',
 	},
 	js: {
-		src: '',
-		dest: '',
-		vendors: {
-			src: ''
-		}
+		vendor: {
+			jquery: 'D:\\repos\\ProgramacionWebIII\\TP\\bower_components\\jquery\\dist\\jquery.js',
+			bootstrap: 'D:\\repos\\ProgramacionWebIII\\TP\\bower_components\\bootstrap\\dist\\js\\bootstrap.js'
+		},
+		src: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\js',
+		dest: 'D:\\repos\\ProgramacionWebIII\\TP\\AlquilaCocheras.Web\\assets\\js\\dist'
 	},
 	img: {
 		src: '',
 		dest: '',
-		vendors: {
+		vendor: {
 			src: ''
 		}
 	}
 };
 
 
-gulp.task('sass', function () {
-  return gulp.src(paths.css.src + '\\main.scss')
-    .pipe(sass().on('error', sass.logError))
-	.pipe(cssmin({ keepSpecialComments: 0 }))
-	.pipe(rename({ suffix: '.min'}))
-    .pipe(gulp.dest(paths.css.dest));
+gulp.task('css', function () {
+	return gulp.src([paths.css.vendor.bootstrap, paths.css.src + '\\main.scss'])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cssmin({ keepSpecialComments: 0 }))
+		.pipe(rename({ suffix: '.min'}))
+		.pipe(gulp.dest(paths.css.dest));
+});
+
+gulp.task('js.vendor', function(){
+	return gulp.src([paths.js.vendor.jquery, paths.js.vendor.bootstrap])
+		.pipe(concatFiles('vendors.js'))
+		.pipe(gulp.dest(paths.js.dest))
+		.pipe(uglify({ compress: true }))
+		.pipe(rename({ basename: 'vendors', suffix: '.min' }))
+		.pipe(gulp.dest(paths.js.dest));
+		
 });
 
 gulp.task('watch', function(){
-	gulp.watch(paths.css.src + '\\*.scss', ['sass']);
+	gulp.watch(paths.css.src + '\\*.scss', ['css']);
+	gulp.watch([paths.js.src + '\\*.js', '!' + paths.js.dist + '*.js'], ['js.vendor']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['css', 'js.vendor', 'watch']);
