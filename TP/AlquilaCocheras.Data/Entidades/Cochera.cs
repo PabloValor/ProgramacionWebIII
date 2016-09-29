c6b701b14eb42b49817ca6099a8adf6d69310837
lauuteeh <lautaro.aragones@live.com.ar>
2016-09-29 13:18:44 -0300

    Entidades  | Cochera

--- TP/AlquilaCocheras.Data/Entidades/Cochera.cs
+++ TP/AlquilaCocheras.Data/Entidades/Cochera.cs
@@ -8,5 +8,18 @@ namespace AlquilaCocheras.Data.Entidades
 {
     class Cochera
     {
+        public int ubicacion { get; set; }
+        public DateTime fecha_inicio { get; set; }
+        public DateTime fecha_fin { get; set; }
+        public DateTime horario_inicio { get; set; }
+        public DateTime horario_fin { get; set; }
+        public String descripcion { get; set; }
+        public int latitud { get; set; }
+        public int longitud { get; set; }
+        public int superficie_m2 { get; set; }
+        public int tipo { get; set; }
+        public int precio_por_hora { get; set; }
+        public int foto { get; set; }
+
     }
 }
