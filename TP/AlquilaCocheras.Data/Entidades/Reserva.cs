df00ac504ea9e2244c1fef3f2d0256a598cf866f
lauuteeh <lautaro.aragones@live.com.ar>
2016-09-29 13:28:34 -0300

    Entidades | Reserva

--- TP/AlquilaCocheras.Data/Entidades/Reserva.cs
+++ TP/AlquilaCocheras.Data/Entidades/Reserva.cs
@@ -8,5 +8,16 @@ namespace AlquilaCocheras.Data.Entidades
 {
     class Reserva
     {
+        public int id_reserva { get; set; }
+        public int id_cliente { get; set; }
+        public int id_cochera { get; set; }
+        public DateTime fecha_inicio { get; set; }
+        public DateTime fecha_fin { get; set; }
+        public DateTime horario_inicio { get; set; }
+        public DateTime horario_fin { get; set; }
+        public int precio_final { get; set; }
+
+
+         
     }
 }
