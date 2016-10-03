018d98b20cc34700207de941335a82f4324eace6
Pablo Valor <pablovalor89@gmail.com>
2016-10-03 16:42:17 -0300

    cambios en Base.Master

--- TP/AlquilaCocheras.Web/MasterPages/Base/Base.Master.cs
+++ TP/AlquilaCocheras.Web/MasterPages/Base/Base.Master.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using AlquilaCocheras.Data.Constantes;
+using AlquilaCocheras.Data.Entidades;
+using AlquilaCocheras.Negocio.Managers;
+using AlquilaCocheras.Negocio.Servicios;
 
 namespace AlquilaCocheras.Web.MasterPages
 {
     public partial class Base : System.Web.UI.MasterPage
     {
+        #region Propiedades
+
+        public Usuario Usuario { get; set; }
+
+        #endregion
+
+        #region Miembros
+
+        private UsuarioService _usuarioService = new UsuarioService();
+
+        #endregion
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (SesionesManager.EsUsuarioLogueado())
+            {
+                Usuario = _usuarioService.ObtenerClientePorId(VariblesSesionManager.Obtener<int>(Constantes.USUARIO_LOGUEADO_ID));
+            }
         }
     }
 }
