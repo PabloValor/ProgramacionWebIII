11ec72be3bd9c99e04cb08544b53807e1eb2d32d
Pablo Valor <pablovalor89@gmail.com>
2016-10-03 17:24:46 -0300

    Cambios en el navbar dependiendo del rol de usuario

--- TP/AlquilaCocheras.Web/MasterPages/Base/Base.Master.cs
+++ TP/AlquilaCocheras.Web/MasterPages/Base/Base.Master.cs
@@ -22,9 +22,11 @@ namespace AlquilaCocheras.Web.MasterPages
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            Usuario = new Usuario();
+
             if (SesionesManager.EsUsuarioLogueado())
             {
-                Usuario = _usuarioService.ObtenerClientePorId(VariblesSesionManager.Obtener<int>(Constantes.USUARIO_LOGUEADO_ID));
+                Usuario = _usuarioService.ObtenerUsuarioPorId(VariblesSesionManager.Obtener<int>(Constantes.USUARIO_LOGUEADO_ID));
             }
         }
     }
